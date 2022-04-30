293f877fcc438eb5d47064cf44198bc0a77279c1
Nathan <nathan.vincent.2.718@gmail.com>
2022-04-29 19:53:30 -0500

    loading about me work

--- lib/views/about_me_view.dart
+++ lib/views/about_me_view.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/constants/portfolio_colors.dart';
+import 'package:portfolio/objects/about_me.dart';
 
 class AboutMeView extends StatefulWidget {
   const AboutMeView({Key? key}) : super(key: key);
@@ -11,36 +15,69 @@ class AboutMeView extends StatefulWidget {
 }
 
 class _AboutMeViewState extends State<AboutMeView> {
+  Future<String> getAboutMes() {
+    return rootBundle.loadString("json/about_me.json");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(10, 50, 10, 10),
-      child: Column(
-        children: [
-          Text("I'm flattered you want to know more about me :)",
-              textAlign: TextAlign.center,
-              style: GoogleFonts.prompt(
-                textStyle: TextStyle(
-                  fontSize: 22.sp,
-                  fontWeight: FontWeight.w800,
-                  color: offWhite,
-                ),
-              )),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-              "I attended Mississippi State Unviersity for seven semesters, majoring in software engineering and mathematics. I finished my SE degree and began working in industry for Camgian. I have plans to return to school in the future to finish my mathematics major and possible pursue a masters or Ph.D. in Computer Science.",
-              textAlign: TextAlign.center,
-              style: GoogleFonts.prompt(
-                textStyle: TextStyle(
-                  fontSize: 18.sp,
-                  fontWeight: FontWeight.w800,
-                  color: offWhite,
-                ),
-              )),
-        ],
-      ),
+    return SingleChildScrollView(
+      child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 50, 10, 10),
+          child: FutureBuilder(
+            future: getAboutMes(),
+            builder: (context, AsyncSnapshot snapshot) {
+              if (!snapshot.hasData) {
+                List<AboutMe> aboutMes = (json
+                    .decode(snapshot.data)
+                    .map((i) => AboutMe.fromJson(i))
+                    .toList());
+
+                print(aboutMes.length);
+
+                List<Widget> widgetStack = [];
+
+                for (int i = 0; i < aboutMes.length; i++) {
+                  widgetStack.add(Text(aboutMes[i].title,
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.prompt(
+                        textStyle: TextStyle(
+                          fontSize: 22.sp,
+                          fontWeight: FontWeight.w800,
+                          color: offWhite,
+                        ),
+                      )));
+                  widgetStack.add(SizedBox(
+                    height: 20,
+                  ));
+                  widgetStack.add(Text(aboutMes[i].description,
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.prompt(
+                        textStyle: TextStyle(
+                          fontSize: 18.sp,
+                          fontWeight: FontWeight.w800,
+                          color: offWhite,
+                        ),
+                      )));
+                  widgetStack.add(SizedBox(
+                    height: 20,
+                  ));
+                }
+
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: widgetStack,
+                );
+              } else {
+                return Container(
+                  width: 500,
+                  height: 500,
+                  color: primaryThemeColor,
+                );
+              }
+            },
+          )),
     );
   }
 }
